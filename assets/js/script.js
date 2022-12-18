337224aa9f206451a0b34d7592269b92e5876272
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-18 20:32:46 +0530

    Content Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,19 @@ $("#txtNumber").on("keyup", function(e) {
     if ($("#cmbInput").val() == 1){
         switch ($("#cmbResult").val()) {
             case "1":
-                console.log(($("#txtNumber").val() >>> 0).toString(2));
+                console.log($("#txtNumber").val());
                 break;
         
             case "2":
-                console.log(($("#txtNumber").val() >>> 0).toString(2))
+                console.log(($("#txtNumber").val() >>> 0).toString(2));
                 break;
 
             case "3":
                 console.log(($("#txtNumber").val() >>> 0).toString(8));
+                break;
+
+            case "4":
+                console.log(parseInt($("#txtNumber").val()).toString(16));
                 break;
         }
     }
