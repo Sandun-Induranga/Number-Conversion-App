9cef714fb20b9c8ce23f5dcfff55c865f196b131
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-18 19:38:45 +0530

    Content Updated

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$("#txtNumber").on("keypress", function(e) {
+    if ($("#cmbInput").val() == 1){
+        switch ($("#txtNumber").val()) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+});
