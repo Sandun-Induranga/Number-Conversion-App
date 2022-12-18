7e8dbaf3fef9f0ca8cf4e99e9f34346105095999
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-18 20:37:21 +0530

    Content Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,45 @@
 $("#txtNumber").on("keyup", function(e) {
-    if ($("#cmbInput").val() == 1){
-        switch ($("#cmbResult").val()) {
-            case "1":
-                console.log($("#txtNumber").val());
-                break;
-        
+
+    switch ($("#cmbInput")) {
+        case "1":
+            switch ($("#cmbResult").val()) {
+                case "1":
+                    console.log($("#txtNumber").val());
+                    break;
+            
+                case "2":
+                    console.log(($("#txtNumber").val() >>> 0).toString(2));
+                    break;
+    
+                case "3":
+                    console.log(($("#txtNumber").val() >>> 0).toString(8));
+                    break;
+    
+                case "4":
+                    console.log(parseInt($("#txtNumber").val()).toString(16));
+                    break;
+            }
+            break;
+    
             case "2":
-                console.log(($("#txtNumber").val() >>> 0).toString(2));
+                switch ($("#cmbResult").val()) {
+                    case "1":
+                        console.log($("#txtNumber").val());
+                        break;
+                
+                    case "2":
+                        console.log(($("#txtNumber").val() >>> 0).toString(2));
+                        break;
+        
+                    case "3":
+                        console.log(($("#txtNumber").val() >>> 0).toString(8));
+                        break;
+        
+                    case "4":
+                        console.log(parseInt($("#txtNumber").val()).toString(16));
+                        break;
+                }
                 break;
-
-            case "3":
-                console.log(($("#txtNumber").val() >>> 0).toString(8));
-                break;
-
-            case "4":
-                console.log(parseInt($("#txtNumber").val()).toString(16));
-                break;
-        }
     }
+
 });
