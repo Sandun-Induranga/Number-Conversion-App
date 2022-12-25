619d92d4cd9b1ebe9d7d9ed3a0eb157e05ad3f59
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-25 20:28:00 +0530

    Case 2 completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,75 @@
-$("#txtNumber").on("keyup", function(e) {
+$("#txtNumber").on("keyup", function() {
 
-    switch ($("#cmbInput")) {
+    switch ($("#cmbInput").val()) {
         case "1":
             switch ($("#cmbResult").val()) {
                 case "1":
-                    console.log($("#txtNumber").val());
+                    $("#txtResult").val($("#txtNumber").val());
                     break;
             
                 case "2":
-                    console.log(($("#txtNumber").val() >>> 0).toString(2));
+                    $("#txtResult").val(($("#txtNumber").val() >>> 0).toString(2));
                     break;
     
                 case "3":
-                    console.log(($("#txtNumber").val() >>> 0).toString(8));
+                    $("#txtResult").val(($("#txtNumber").val() >>> 0).toString(8));
                     break;
     
                 case "4":
-                    console.log(parseInt($("#txtNumber").val()).toString(16));
+                    $("#txtResult").val(parseInt($("#txtNumber").val()).toString(16));
                     break;
             }
             break;
     
-            case "2":
-                switch ($("#cmbResult").val()) {
-                    case "1":
-                        console.log($("#txtNumber").val());
-                        break;
-                
-                    case "2":
-                        console.log(($("#txtNumber").val() >>> 0).toString(2));
-                        break;
-        
-                    case "3":
-                        console.log(($("#txtNumber").val() >>> 0).toString(8));
-                        break;
-        
-                    case "4":
-                        console.log(parseInt($("#txtNumber").val()).toString(16));
-                        break;
-                }
+        case "2":
+
+            let res = parseInt($("#txtNumber").val(), 2);
+
+            switch ($("#cmbResult").val()) {
+
+                case "1":
+                    $("#txtResult").val(res);
+                    break;
+            
+                case "2":
+                    $("#txtResult").val($("#txtNumber").val());
+                    break;
+    
+                case "3":
+                    $("#txtResult").val((res >>> 0).toString(8));
+                    break;
+    
+                case "4":
+                    $("#txtResult").val(parseInt(res).toString(16));
+                    break;
+            }
+
+            break;
+
+        case "2":
+
+        res = parseInt($("#txtNumber").val(), 8);
+
+        switch ($("#cmbResult").val()) {
+
+            case "1":
+                $("#txtResult").val(res);
                 break;
-    }
+            
+            case "2":
+                $("#txtResult").val($("#txtNumber").val());
+                break;
+    
+            case "3":
+                $("#txtResult").val((res >>> 0).toString(8));
+                break;
+    
+            case "4":
+                $("#txtResult").val(parseInt(res).toString(16));
+                break;
+        }
+
+        break;
+}
 
 });
